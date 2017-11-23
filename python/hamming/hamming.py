fb4f38f74dfd0857321650fea4dde176a79f80a7
varunbhanot <varunbhanot@gmail.com>
2017-11-23 11:27:51 +0530

    hamming  excercise with use of zip

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,11 +1,5 @@
 def distance(strand_a, strand_b):
     if(len(strand_a) != len(strand_b)):
         raise ValueError
-    count = 0
-    loop = 0
-    for c in strand_a:
-        if strand_b[loop] != c:
-            count+=1
-        loop+=1
-    return count
+    return sum(1 for x,y in zip(strand_a,strand_b) if x!=y)
 
