4cfbc0c08b803616b001f99e5647b7e0d6fdc8f6
varunbhanot <varunbhanot@gmail.com>
2017-11-23 10:07:02 +0530

    pangram excercise

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,8 +2,6 @@ import itertools
 def is_pangram(sentence):
     count = 0
     final_string = ''.join(ch for ch, _ in itertools.groupby(sentence))
-    print final_string
+    return len(final_string) == 26
 
-if __name__ == '__main__':
-    is_pangram("abcdddee")
 
