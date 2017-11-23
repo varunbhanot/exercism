ae6f91cc3970215e315026673fd8c4fec56fa8a3
varunbhanot <varunbhanot@gmail.com>
2017-11-23 10:12:38 +0530

    pangram excercise

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,7 +1,7 @@
 import itertools
 def is_pangram(sentence):
-    count = 0
-    final_string = ''.join(ch for ch, _ in itertools.groupby(sentence))
-    return len(final_string) == 26
+    sentence = str(sentence)
+    final_set = set(c.lower() for c in sentence if c.isalpha())
+    return len(final_set) == 26
 
 
